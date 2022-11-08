70d2e6a7cf2368b9afcca57003b73135afbe3908
Robin Richardson <raarichardson@gmail.com>
2022-11-08 17:06:36 +0100

    Fix linting errors

--- niceday-api/service/FilesSharingService.js
+++ niceday-api/service/FilesSharingService.js
@@ -1,7 +1,6 @@
 const { SenseServer, FileChat } = require('@sense-os/goalie-js');
-var FormData = require('form-data');
-const fs = require('fs');
-require('isomorphic-fetch');
+const FormData = require('form-data');
+const fetch = require('isomorphic-fetch');
 
 /**
  * Upload a file
@@ -10,37 +9,36 @@ require('isomorphic-fetch');
  * @param body - The node.js express body object
  * */
 exports.uploadFile = (req, body) => new Promise((resolve, reject) => {
-      const fileChat = new FileChat()
+  const fileChat = new FileChat();
 
-      fileChat.init(SenseServer.Alpha)
-      var baseUrl = fileChat.baseUrl()
+  fileChat.init(SenseServer.Alpha);
+  const baseUrl = fileChat.baseUrl();
 
-      var token = req.app.get('token')
-      var requestHeaders = new Headers();
-      requestHeaders.append("Authorization", "Token "+token);
+  const token = req.app.get('token');
+  const requestHeaders = new fetch.Headers();
+  requestHeaders.append('Authorization', `Token ${token}`);
+  const formData = new FormData();
+  formData.append('file', req.files[0].buffer, {
+    filepath: body.file_path,
+  });
+  formData.append('name', req.files[0].originalname);
+  formData.append('receiver', Number(body.receiver_id));
 
-      var formData = new FormData();
-      formData.append("file", req.files[0].buffer,{
-        filepath: body.file_path
-      });
-      formData.append("name", req.files[0].originalname);
-      formData.append("receiver", Number(body.receiver_id));
+  const requestOptions = {
+    method: 'POST',
+    headers: requestHeaders,
+    body: formData,
+    redirect: 'follow',
+  };
 
-      var requestOptions = {
-        method: 'POST',
-        headers: requestHeaders,
-        body: formData,
-        redirect: 'follow'
-      };
-
-      fetch(baseUrl + "?Authorization="+token, requestOptions)
-        .then(response => response.text())
-        .then((result) => {
-            console.log('Successfully uploaded file ', result)
-            resolve(result)
-        })
-        .catch((err) => {
-          console.log('Error:', err);
-          reject(err);
-        });
+  fetch(`${baseUrl}?Authorization=${token}`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      console.log('Successfully uploaded file ', result);
+      resolve(result);
+    })
+    .catch((err) => {
+      console.log('Error:', err);
+      reject(err);
     });
+});
